d74e913f8ef8dcb7644d9cc3494a6e646948a795
veronicarubim <veronica.rubim97@gmail.com>
2023-01-09 16:04:54 -0300

    Arquivos upados

---
+++ src/pages/Carrinho.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Cart from '../components/Carrinho/Cart'
+import Footer from '../components/Footer/Footer'
+
+const Carrinho = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Cart/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Carrinho
